97bedb2e5da63a8bcdfc88647038c2ed918b7bdc
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-01-28 12:55:37 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -85,7 +85,8 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
             ]
             for file in files
@@ -94,11 +95,12 @@ async def next_page(bot, query):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                 ),
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
             ]
             for file in files
         ]
@@ -661,8 +663,8 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
             ]
             for file in files
         ]
@@ -670,11 +672,11 @@ async def auto_filter(client, msg, spoll=False):
         btn = [
             [
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
                 InlineKeyboardButton(
-                    text=f"🔖{get_size(file.file_size)}🔮{file.file_name}", callback_data=f'files#{file.file_id}'
-                ),
+                    text=f"[{get_size(file.file_size)}] {file.file_name}", 
+                    url=(f"https://telegram.dog/{temp.U_NAME}?start=files_{file.file_id}")
             ]
             for file in files
         ]
