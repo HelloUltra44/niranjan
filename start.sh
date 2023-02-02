180720a42085c7aee5280a6bc9b3b95826440039
HelloUltra44 <122900262+HelloUltra44@users.noreply.github.com>
2023-02-02 19:33:29 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /niranjan
 fi
-cd /Eva
+cd /niranjan
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
